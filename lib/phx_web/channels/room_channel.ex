e3b0da69d063251718d7d6a8983fdda176f9eaad
Kirill <gluck1986@gmail.com>
2019-02-18 13:14:35 +0400

    tests

--- lib/phx_web/channels/room_channel.ex
+++ lib/phx_web/channels/room_channel.ex
@@ -1,9 +1,26 @@
-defmodule Phx.RoomChannel do
-  use Phoenix.Channel
+defmodule PhxWeb.RoomChannel do
+  use PhxWeb, :channel
 
-  def handle_in("ping", payload, socket) do
-    {:reply, {:ok, payload}, socket}
-  end
+#  def join("room:lobby", payload, socket) do
+#    if authorized?(payload) do
+#      {:ok, socket}
+#    else
+#      {:error, %{reason: "unauthorized"}}
+#    end
+#  end
+
+#  # Channels can be used in a request/response fashion
+#  # by sending replies to requests from the client
+#  def handle_in("ping", payload, socket) do
+#    {:reply, {:ok, payload}, socket}
+#  end
+#
+#  # It is also common to receive messages from the client and
+#  # broadcast to everyone in the current topic (room:lobby).
+#  def handle_in("shout", payload, socket) do
+#    broadcast(socket, "shout", payload)
+#    {:noreply, socket}
+#  end
 
   def join("room:inn", _message, socket) do
     {:ok, socket}
@@ -44,5 +61,10 @@ defmodule Phx.RoomChannel do
     end
 
     {:noreply, socket}
+  end
+
+  # Add authorization logic here as required.
+  defp authorized?(_payload) do
+    true
   end
 end
---
+++ test/phx_web/channels/room_channel_test.exs
@@ -0,0 +1,52 @@
+defmodule PhxWeb.RoomChannelTest do
+  use PhxWeb.ChannelCase
+  import Ecto.Query
+
+  #  setup do
+  #    {:ok, _, socket} =
+  #      socket(PhxWeb.UserSocket, "user_id", %{some: :assign})
+  #      |> subscribe_and_join(PhxWeb.RoomChannel, "room:inn")
+  #
+  #    {:ok, socket: socket}
+  #  end
+
+  setup do
+    {:ok, _, socket} =
+      socket(PhxWeb.UserSocket, "user_id", %{some: :assign})
+      |> subscribe_and_join(PhxWeb.RoomChannel, "room:inn")
+
+    {:ok, socket: socket}
+  end
+
+  ##
+  ##  test "ping replies with status ok", %{socket: socket} do
+  ##    ref = push socket, "ping", %{"hello" => "there"}
+  ##    assert_reply ref, :ok, %{"hello" => "there"}
+  ##  end
+  ##
+  ##  test "shout broadcasts to room:lobby", %{socket: socket} do
+  ##    push socket, "shout", %{"hello" => "all"}
+  ##    assert_broadcast "shout", %{"hello" => "all"}
+  ##  end
+  ##
+  ##  test "broadcasts are pushed to the client", %{socket: socket} do
+  ##    broadcast_from! socket, "broadcast", %{"some" => "data"}
+  ##    assert_push "broadcast", %{"some" => "data"}
+  ##  end
+
+  test "new_inn broadcasts to room:inn", %{socket: socket} do
+    push(socket, "new_inn", %{"body" => "all"})
+    broadcast_from! socket, "error", %{"body" => "ИНН должен состоять из чисел и иметь 10 или 12 занков"}
+    push socket, "new_inn", %{"body" => "6449013711"}
+    broadcast_from! socket, "result", %{"body" => "6449013718"}
+    ## assert_push "new_inn", %{"body" => "64490137"}
+    #    ref = push(socket, "new_inn", %{"body" => "v"})
+    #    assert_broadcast "new_inn", %{"body" => "6449013711"}
+    #    ##assert_push "new_inn", %{"body" => "6449013711"}
+#        inn_check = Phx.InnCheck
+#        |> Phx.Repo.count()
+#inn_check  = Phx.Repo.one(from p in Phx.InnCheck, select: count("*"))
+#        assert inn_check ===1
+#        assert inn_check.inn === "6449013711"
+  end
+end
