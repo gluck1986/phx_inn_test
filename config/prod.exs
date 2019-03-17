5c47c4df436b6a28e8e03263befd26eee37fc4a4
Kirill <gluck1986@gmail.com>
2019-03-17 15:34:14 +0400

    heroku

--- config/prod.exs
+++ config/prod.exs
@@ -20,6 +20,11 @@ config :phx, PhxWeb.Endpoint,
 # Do not print debug messages in production
 config :logger, level: :info
 
+config :hello, Phx.Repo,
+       url: System.get_env("DATABASE_URL"),
+       pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
+       ssl: true
+
 # ## SSL Support
 #
 # To get SSL working, you will need to add the `https` key
