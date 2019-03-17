01f9e98e08c2884bd94ac6272b77071142c68952
Kirill <gluck1986@gmail.com>
2019-03-17 20:32:07 +0400

    secret

--- config/prod.exs
+++ config/prod.exs
@@ -15,7 +15,8 @@ config :phx, PhxWeb.Endpoint,
   force_ssl: [rewrite_on: [:x_forwarded_proto]],
   pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
   ssl: true,
-  cache_static_manifest: "priv/static/cache_manifest.json"
+  cache_static_manifest: "priv/static/cache_manifest.json",
+  secret_key_base: Map.fetch!(System.get_env(), "SECRET_KEY_BASE")
 
 # Do not print debug messages in production
 config :logger, level: :info
